246622ad2b7d2c560aa9b947baa57fe5c4f10e88
Jordan <jordan@jpdesigning.com>
2022-12-06 08:59:14 -0500

    fix(grep): update cypress-grep references & docs & add e2e tests

--- npm/grep/cypress/e2e/omit-and-skip-spec.js
+++ npm/grep/cypress/e2e/omit-and-skip-spec.js
@@ -1,8 +1,10 @@
 /// <reference path="../../src/index.d.ts" />
 
 // @ts-check
+
 describe('Page', () => {
   describe('List', { tags: ['@us1'] }, () => {
+    // NOTE: Skipped to test these
     it.skip('first test', () => {})
     it('second test', () => {})
     it('third test', () => {})
--- npm/grep/cypress/support/e2e.js
+++ npm/grep/cypress/support/e2e.js
@@ -1,7 +1,7 @@
-// @ts-check
 /// <reference types="cypress" />
 
+require('./commands')
+
 import cypressGrep from '../../src/support'
-// register the grep feature
-// https://github.com/cypress-io/cypress-grep
+
 cypressGrep()
